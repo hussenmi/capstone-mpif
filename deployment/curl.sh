2663347a36b62913c65baedea7176e30348d1a14
Hussen Mohammed Ibrahim <hussenmohammedak@gmail.com>
2024-02-25 19:47:47 +0000

    Updated README

--- deployment/curl.sh
+++ deployment/curl.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-# Replace with the appropriate URL of your Flask app
 URL="http://localhost:5000/"
 
-# Replace with the paths to the CSV files you want to upload
-COORDINATES_PATH="test_files/SP41_2_X2Y8/BaselTMA_SP41_2_X2Y8_coords.csv"
-EXPRESSIONS_PATH="test_files/SP41_2_X2Y8/BaselTMA_SP41_2_X2Y8_expression.csv"
+COORDINATES_PATH="test_files/SP41_129_X7Y8/BaselTMA_SP41_129_X7Y8_coords.csv"
+EXPRESSIONS_PATH="test_files/SP41_129_X7Y8/BaselTMA_SP41_129_X7Y8_expression.csv"
 
-# Perform the HTTP POST request
 curl -X POST -F "coordinates=@$COORDINATES_PATH" -F "expressions=@$EXPRESSIONS_PATH" $URL
