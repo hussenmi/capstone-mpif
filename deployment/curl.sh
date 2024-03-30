03366510f5d1ccec6b35fe5745a11b91d0bfbd8b
Hussen Mohammed Ibrahim <hussenmohammedak@gmail.com>
2024-03-30 14:44:48 +0000

    Changed port for curl test

--- deployment/curl.sh
+++ deployment/curl.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-URL="http://localhost:5000/"
+URL="http://localhost:5001/"
 
 COORDINATES_PATH="test_files/SP41_129_X7Y8/BaselTMA_SP41_129_X7Y8_coords.csv"
 EXPRESSIONS_PATH="test_files/SP41_129_X7Y8/BaselTMA_SP41_129_X7Y8_expression.csv"
